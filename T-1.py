025f7b5663bb6d1c653e34a36802de7ce511c309
Raveena Mathkar <raveenamathkar@gmail.com>
2020-07-22 00:57:31 +0530

    plotting graphs

--- T-1.py
+++ T-1.py
@@ -1,10 +1,12 @@
-
 import json
 import matplotlib.pyplot as plt
 
+fig, ax = plt.subplots()
+
 with open('C:/xampp/htdocs/2020-interns/data.json') as f:
   data = json.load(f)
-
 for i in data["rates"].keys():
     inr = data["rates"][i]['INR']    
-    print(inr)
+    eur=data["base"] 
+    ax.plot(inr, eur, color='red', linestyle='--', linewidth=2, marker='s')
+    ax.show()
